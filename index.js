d576dc2e6c76e154880611a69c07383e6ad9925f
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-13 13:40:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//currying function
+const multiArgFunction = (a, b, c) => a + b + c;
+console.log(multiArgFunction(1, 2, 3)); // 6
+
+const curryUnaryFunction = (a) => (b) => (c) => a + b + c;
+curryUnaryFunction(1); // returns a function: b => c =>  1 + b + c
+curryUnaryFunction(1)(2); // returns a function: c => 3 + c
+curryUnaryFunction(1)(2)(3); // returns the number 6
+
 //higher order function
 const firstOrderFunc = () =>
   console.log("Hello, I am a First order function");
