e825e71dc7241e4b4c9d571f9662b3b67e0981ab
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-13 13:24:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// diffrence between call apply and bind methods
+
+// call() method
+   //it invokes the function with this value and allow to pass in arguments as one by one.
+var employee1 = { firstName: "John", lastName: "Rodson" };
+var employee2 = { firstName: "Jimmy", lastName: "Baily" };
+
+function invite(greeting1, greeting2) {
+  console.log(
+    greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+  );
+}
+
+invite.call(employee1, "Hello", "How are you?"); // Hello John Rodson, How are you?
+invite.call(employee2, "Hello", "How are you?"); // Hello Jimmy Baily, How are you?
+
+
+//apply() method
+    //it invokes the function with this value and allows ro pass in arguments as an array.   
+var employee1 = { firstName: "john", lastName: "radson" };
+var employee2 = { firstName: "jimmy", lastName: "bally" };
+
+ function invite(greeting1, greeting2 ) {
+    console.log(
+        greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+    );
+ }
+invite.apply(employee1, ["hello", "how are you?"]); //hello john radson, how are you?
+invite.apply(employee2, ["hello", "how are you?"]); //hello jimmy bally, how are you?
+
+
+//bind() method
+    //it returns a new function and allows you to pass number of arguments
+var employee1 = {firstName: "john", lastName: "radson" };
+var employee2 = {firstName: "jimmy", lastName: "bally" };
+
+function invite(greeting1, greeting2) {
+    console.log(
+        greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2 
+    );
+}
+var inviteEmployee1 = invite.bind(employee1);
+var inviteEmployee2 = invite.bind(employee2);
+
+inviteEmployee1("hello", "how are you?"); // hello John Rodson, How are you?
+inviteEmployee2("hello", "how are you?"); // hello jimmy bally, how are you?
