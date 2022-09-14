ab2c2c4fbe5d36ac46fa13efd11529123997672d
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-14 13:34:30 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//array splice method
+let arrayIntegersOriginals1 = [1, 2, 3, 4, 5];
+let arrayIntegersOriginals2 = [1, 2, 3, 4, 5];
+let arrayIntegersOriginals3 = [1, 2, 3, 4, 5];
+let arrayIntegerso1 = arrayIntegersOriginals1.splice(0,2);// return [1,2] original array [3,4,5]
+let arrayIntegerso2 = arrayIntegersOriginals2.splice(3);//return [4,5] original array [1,2,3]
+let arrayIntegerso3 = arrayIntegersOriginals3.splice(3, 1, "a", "b", "c");//return [4] original array [1, 2, 3, "a", "b", "c", 5]
+
+
+//array slice method
+let arrayIntegers = [1, 2, 3, 4, 5];
+let arrayIntegers1 = arrayIntegers.slice(0,2);// return [1,2]
+let arrayintegers2 = arrayIntegers.slice(2,3);// return [3]
+let arrayIntegers3 = arrayIntegers.slice(4)// return [5]
+
+
+//pure function
+//Impure
+let numberArray = [];
+const impureAddNumber = (number) => numberArray.push(number);
+//Pure
+const pureAddNumber = (number) => (argNumberArray) =>
+  argNumberArray.concat({number});
+
+
+//Display the results
+console.log(impureAddNumber(6)); // returns 1
+console.log(numberArray); // returns [6]
+console.log(pureAddNumber(7)(numberArray)); // returns [6, 7]
+console.log(numberArray); // returns [6]
+
+
+//unary function
+const unaryFunction= (a) => console.log(a+ 10);// Add 10 to the given argument and display the value
+
 //currying function
 const multiArgFunction = (a, b, c) => a + b + c;
 console.log(multiArgFunction(1, 2, 3)); // 6
@@ -59,3 +94,15 @@ var inviteEmployee2 = invite.bind(employee2);
 
 inviteEmployee1("hello", "how are you?"); // hello John Rodson, How are you?
 inviteEmployee2("hello", "how are you?"); // hello jimmy bally, how are you?
+
+//closures
+function Welcome(name) {
+    var greetingInfo = function (message) {
+      console.log(message + " " + name);
+    };
+    return greetingInfo;
+  }
+  var myFunction = Welcome("John");
+  myFunction("Welcome "); //Output: Welcome John
+  myFunction("Hello Mr."); //output: Hello Mr.John
+
