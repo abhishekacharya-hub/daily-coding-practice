75d38d2a00d9cbf328e68a67f5813e035a595b6e
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-24 12:28:52 +0530

    firstcommit

--- index.js
+++ index.js
@@ -831,3 +831,15 @@ async function fetchMethod() {
   }
 }
 fetchMethod();
+//------------------------------------------------------------------------------------
+
+//XMLHttpRequest():
+const xhr = new XMLHttpRequest;
+xhr.onreadystatechange = function() {
+  if(this.readystate === 4 && this.status === 200) {
+    console.log(this.responseText);
+  }
+};
+xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true );
+xhr.setRequestHeader("content-Type", "application/x-www-form-urlencoded");
+xhr.send();
