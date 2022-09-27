c25252e42145a43025ab8cedf8ce325eadaf9d7e
abhishek acharya <85024420+abhishekacharya-hub@users.noreply.github.com>
2022-09-27 18:56:16 +0530

    firstcommit

--- interview.js
+++ interview.js
@@ -14,6 +14,8 @@ console.log(object1);
 //what is prototype chain
 //prototype chain is this is usually build a new object on the top of existing obejct.
 
+//=========================================================================================
+
 //differrence between call apply and blind
 //call()
 let teacher1 = {firstName: "priya", lastName: "sachdeva"}
@@ -38,3 +40,39 @@ invite.apply(teacher3, ["hey", "how are you?"]);
 invite.apply(teacher4, ["hey", "how are you?"]);
 
 //bind()
+let teacher5 = {firstName: "sherya", lastName: "chopra"}
+let teacher6 = {firstName: "madhu", lastName: "shree"}
+function invite(greeting1,greeting2) {
+    console.log(
+        greeting1 + " " + this.firstName + this.lastName + " " + greeting2
+    );
+}
+let inviteTeacher1 = invite.bind(teacher5);
+let inviteTeacher2 = invite.bind(teacher6);
+inviteTeacher1( "hey", "how are you?");
+inviteTeacher2( "hey", "hwo are you?");
+
+//====================================================================================
+
+//what is json
+//json is javascript object notation it mostly knows for transmit data across netwroks
+//JSON.parse(text);
+//JSON.stringify(object);
+
+//====================================================================================
+
+//purpose of array slice() method
+//it returned the selected items from an array as a new array
+let arrayInteger = [1,2,3,4,5];
+let arrayInt1 = arrayInteger.slice(0,2); // [1,2]
+let arrayInt2 = arrayInteger.slice(1,3); // [2,3,4]
+let arrayInt3 = arrayInteger.slice(4); // [5]
+
+//purpose of splice() method
+//it either add or remove items and return the removed item
+let arrayInt4 = arrayInteger.splice(0,2); //return [1,2] original array [3,4,5]
+let arrayInt5 = arrayInteger.splice(3); //return [4,5] original array [1,2,3]
+let arrayInt6 = arrayInteger.splice(3, 1, "a", "b", "c") //return [4] orginal array [1,2,3,"a","b","c",5]
+
+//========================================================================================================
+
